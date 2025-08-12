9c957df8b2518e2475903003975c2680cd1b2419
FriMohamed <moh2med.fri@gmail.com>
2025-08-12 23:44:19 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-mod road;
-use road::*;
 use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
+// use rand::Rng;
+use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
@@ -17,10 +18,9 @@ pub fn main() {
  
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
-
-    let mut intersection = Intersection::new(Road::new(width as i32, Direction::Horizontal, 35),
-            Road::new(height as i32, Direction::Vertical, 35)
-    );
+    let mut cars = Vec::new();
+    // let mut rng = rand::rng();
+    let directions = ["left", "right", "forward"];
     
     
     'running: loop {
@@ -33,49 +33,139 @@ pub fn main() {
                     break 'running
                 },
                 Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
-                    let car = Car::new(width as i32/2 + 1, height as i32 - 33, 33, Color::RGB(0, 0, 255), 0, -2);
-                    intersection.road2.lane2.cars.push(car);
+                    let i = rand::random_range(0..2);
+                    let car = Car::new(width as i32/2 + 1, height as i32 - 33, 33, "up".to_string(), directions[i].to_string(), Color::RGB(0, 0, 255));
+                    println!("{}", car.dir_atfer_int);
+                    cars.push(car);
                 },
                 Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
-                    let car = Car::new(width as i32/2 - 33 , 0, 33, Color::RGB(0, 0, 255), 0, 2);
-                    intersection.road2.lane1.cars.push(car);
+                    let i = rand::random_range(0..2);
+                    let car = Car::new(width as i32/2 - 33 , 0, 33, "down".to_string(), directions[i].to_string(), Color::RGB(0, 0, 255));
+                    println!("{}", car.dir_atfer_int);
+                    cars.push(car);
                 },
                 Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
-                    let car = Car::new(0, height as i32/2 + 1, 33, Color::RGB(0, 0, 255), 2, 0);
-                    intersection.road1.lane2.cars.push(car);
+                    let i = rand::random_range(0..2);
+                    let car = Car::new(0, height as i32/2 + 1, 33, "right".to_string(), directions[i].to_string(), Color::RGB(0, 0, 255));
+                    println!("{}", car.dir_atfer_int);
+                    cars.push(car);
                 },
                 Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
-                    let car = Car::new(width as i32 - 33, height as i32/2 - 33, 33, Color::RGB(0, 0, 255), -2, 0);
-                    intersection.road1.lane1.cars.push(car);
+                    let i = rand::random_range(0..2);
+                    let car = Car::new(width as i32 - 33, height as i32/2 - 33, 33, "left".to_string(), directions[i].to_string(), Color::RGB(0, 0, 255));
+                    println!("{}", car.dir_atfer_int);
+                    cars.push(car);
                 },
                 _ => {}
             }
         }
 
-
-        intersection.draw(&mut canvas);
-        intersection.road2.lane2.cars.iter_mut().enumerate().for_each(|(i, car)| {
+        draw_roads(&mut canvas, width as i32, height as i32, 35);
+        cars.iter_mut().for_each(|car| {
             car.draw(&mut canvas);
-            car.mo_ve(intersection.road2.len, i);
-        });
-
-        intersection.road2.lane1.cars.iter_mut().enumerate().for_each(|(i, car)| {
-            car.draw(&mut canvas);
-            car.mo_ve(intersection.road2.len, i);
-        });
-
-        intersection.road1.lane1.cars.iter_mut().enumerate().for_each(|(i, car)| {
-            car.draw(&mut canvas);
-            car.mo_ve(intersection.road2.len, i);
-        });
-
-        intersection.road1.lane2.cars.iter_mut().enumerate().for_each(|(i, car)| {
-            car.draw(&mut canvas);
-            car.mo_ve(intersection.road2.len, i);
+            car.mo_ve(width as i32, height as i32);
         });
 
 
         canvas.present();
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn draw_roads(canvas:  &mut WindowCanvas, width: i32, height: i32, lane_width: i32) {
+    let cx = width / 2;
+    let cy = height / 2;
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    let _ = canvas.draw_line((cx - lane_width , 0_i32), (cx - lane_width, cy*2));
+    let _ = canvas.draw_line((cx + lane_width, 0_i32), (cx + lane_width, cy * 2));
+    let _ = canvas.draw_line((0_i32, cy - lane_width), (cx * 2, cy - lane_width));
+    let _ = canvas.draw_line((0_i32, cy + lane_width), (cx * 2, cy + lane_width));
+        
+    canvas.set_draw_color(Color::RGB(169, 169, 169));
+    let _ = canvas.draw_line((0, cy), (cx * 2, cy));
+    let _ = canvas.draw_line((cx, 0), (cx, cy * 2));
+}
+
+struct Car {
+    x: i32,
+    y: i32,
+    size: u32, 
+    direction: String,
+    dir_atfer_int: String,
+    speed: i32,
+    color: Color,
+    is_changed: bool
+}
+
+impl Car {
+    fn new(x: i32, y: i32, size: u32, direction: String, dir_atfer_int: String, color: Color) -> Self {
+        Self {
+            x,
+            y,
+            size, 
+            direction,
+            dir_atfer_int,
+            color,
+            speed: 2,
+            is_changed: false
+        }
+    }
+
+    fn draw(&self, canvas:  &mut WindowCanvas) {
+        let rect = Rect::new(self.x, self.y, self.size, self.size);
+        canvas.set_draw_color(self.color);
+        canvas.fill_rect(rect).unwrap();
+    }
+
+    fn mo_ve(&mut self, w: i32, h: i32) {
+        if self.direction == "up" {
+            if !self.is_changed && self.dir_atfer_int == "right" && self.y - self.speed < h / 2 {
+                self. direction = "right".to_string();
+                self.is_changed = true;
+            } else if !self.is_changed && self.dir_atfer_int == "left" && self.y - self.speed < (h / 2) - 35 {
+                self. direction = "left".to_string();
+                self.is_changed = true;
+            } else {
+                self.y -= self.speed;
+            }
+        }
+
+        if self.direction == "down" {
+            if !self.is_changed && self.dir_atfer_int == "right" && self.y + self.speed > h / 2 - 35 {
+                self. direction = "left".to_string();
+                self.is_changed = true;
+            } else if !self.is_changed && self.dir_atfer_int == "left" && self.y + self.speed > (h / 2) {
+                self. direction = "right".to_string();
+                self.is_changed = true;
+            } else {
+                self.y += self.speed;
+            }
+        }
+
+        if self.direction == "right" {
+            if !self.is_changed && self.dir_atfer_int == "left" && self.x + self.speed > w / 2 {
+                self. direction = "up".to_string();
+                self.is_changed = true;
+            } else if !self.is_changed && self.dir_atfer_int == "right" && self.x + self.speed > (w / 2) - 35 {
+                self. direction = "down".to_string();
+                self.is_changed = true;
+            } else {
+                self.x += self.speed;
+            }
+        }
+
+         if self.direction == "left" {
+            if !self.is_changed && self.dir_atfer_int == "right" && self.x - self.speed < w / 2 {
+                self. direction = "up".to_string();
+                self.is_changed = true;
+            } else if !self.is_changed && self.dir_atfer_int == "left" && self.x - self.speed < (w / 2) - 35 {
+                self. direction = "down".to_string();
+                self.is_changed = true;
+            } else {
+                self.x -= self.speed;
+            }
+        }
+
     }
 }
